5a27645dedfe74670a1140fff12d7f4b7028fbf5
cetvrtak <59363714+cetvrtak@users.noreply.github.com>
2024-05-18 19:34:02 +0200

    Send request to DaData

--- app/page.js
+++ app/page.js
@@ -4,8 +4,31 @@ import React, { useState } from 'react';
 const HomePage = () => {
   const [query, setQuery] = useState('')
 
-  function handleSubmit(e) {
+  async function handleSubmit(e) {
     e.preventDefault()
+
+    var url = "http://suggestions.dadata.ru/suggestions/api/4_1/rs/findById/party";
+    var token = "237a7776db5718ec773b4319a288282fc9723df2";
+    // var query = "7707083893";
+
+    var options = {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "Authorization": "Token " + token
+      },
+      body: JSON.stringify({ query: query })
+    }
+
+    try {
+      const response = await fetch(url, options);
+      const result = await response.text();
+      console.log(result);
+    } catch (error) {
+      console.log("error", error);
+    }
   }
 
   return (
